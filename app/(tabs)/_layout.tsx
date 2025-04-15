3fa00dcb88f44717f510197d19dd52dfeae279ac
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-04-15 12:18:55 +0200

    configure tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const _layout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen 
+            name="Index" 
+            options={{ 
+                title:"Home",
+                headerShown: false
+            }} 
+        />
+        <Tabs.Screen 
+            name="Search" 
+            options={{ 
+                title:"Search",
+                headerShown: false
+            }} 
+        />
+        <Tabs.Screen 
+            name="Saved" 
+            options={{ 
+                title:"Saved",
+                headerShown: false
+            }} 
+        />
+        <Tabs.Screen 
+            name="Profile" 
+            options={{ 
+                title:"Profile",
+                headerShown: false
+            }} 
+        />
+    </Tabs>
+  )
+}
+
+export default _layout
