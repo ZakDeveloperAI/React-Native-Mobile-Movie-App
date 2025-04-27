25b508ed95c58e0d55027aec0b37d1dacfb40372
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-04-27 19:21:46 +0200

    app icon splash, and index UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ const TabIcon=({focused,icon,title}:any)=>{
             <ImageBackground
                             source={images.highlight}
                             className='flex flex-row w-full 
-                            flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center
+                            flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center
                             rounded-full overflow-hidden'
                         >
                             <Image source={icon}
@@ -33,7 +33,29 @@ const TabIcon=({focused,icon,title}:any)=>{
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+            },
+            tabBarStyle: {
+                backgroundColor: "#0f0D23",
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: "absolute",
+                overflow: "hidden",
+                borderWidth: 1,
+                borderColor: "#0f0D23",
+            }
+
+        }}
+    >
         <Tabs.Screen name="index"  
         options={{ 
             title: 'Home',
