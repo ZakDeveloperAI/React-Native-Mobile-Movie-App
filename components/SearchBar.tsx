0a2eae90bcb6ff52a46483bc05fe2e93a05ddeee
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-04-27 19:26:27 +0200

    SearchBar Component and router config

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,19 @@
 import { View, Text, Image, TextInput } from 'react-native'
 import React from 'react'
 import { icons } from '@/constants/icons'
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
 
-const SearchBar = () => {
+const SearchBar = ({placeholder,onPress}:Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image source={icons.search} className='size-5' 
         resizeMode='contain' tintColor="#ab8bff"/>
         <TextInput
-            onPress={()=>{}}
-            placeholder='Search for anything...'
+            onPress={onPress}
+            placeholder={placeholder}
             value=''
             onChangeText={()=>{}}
             placeholderTextColor={"#a8b5db"}
