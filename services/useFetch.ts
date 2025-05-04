cababe9a1854b0b11b1248074d9819a51f0d9883
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-05-04 16:01:33 +0200

    configured api rest to fetch latest and searched movies

---
+++ services/useFetch.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useFetch=<T>(fetchfunction:()=>Promise<T>, autofetch=true)=>{
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+            const result = await fetchfunction();
+            setData(result);
+        } catch (err) {
+            // @ts-ignore
+            setError(err instanceof Error ? err: new Error("An error occurred"));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const reset=()=>{
+        setData(null);
+        setLoading(false);
+        setError(null);
+    }
+
+    useEffect(() => {
+        if (autofetch) {
+            fetchData();
+        }
+    },[])
+
+    return { data, loading, error, refetch: fetchData, reset };
+}
+
+export default useFetch;
